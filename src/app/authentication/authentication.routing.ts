af1ac86c524bce4b733b0368c86673d609aae4e2
Biren2055 <129747642+Biren2055@users.noreply.github.com>
2023-07-18 15:52:25 +0100

    change

--- src/app/authentication/authentication.routing.ts
+++ src/app/authentication/authentication.routing.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from '../landing/landing.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AuthenticationComponent } from './authentication.component';
 
 const routes: Routes = [
     {
-        path:'',component:LandingComponent
+        path:'',component:AuthenticationComponent
     },
     {
         path:'sign-up',component:SignUpComponent
